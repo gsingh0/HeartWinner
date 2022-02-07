e421df346215c7e46b04fd1d84dcd76bee5cc9df
Gurnoor Singh <gurnoorsingh@Gurnoors-MacBook-Pro.local>
2022-02-07 02:09:25 -0500

    firebase auth + login screen

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/ui/screens/home/Home';
+import Login from './src/ui/screens/login/Login';
+import * as firebase from 'firebase/app';
+import apiKeys from './config/keys';
 
 export default function App() {
+  if (!firebase.getApps().length) {
+    console.log('Connected with Firebase')
+    firebase.initializeApp(apiKeys.firebaseConfig);
+  }
+
   return (
-    <Home />
+    <Login />
+    // <Home />
   );
 }
 
