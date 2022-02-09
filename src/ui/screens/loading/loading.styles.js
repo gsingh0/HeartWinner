21fc937b7aefc9922e855acfb771a56ffd3bc191
Gurnoor Singh <gurnoorsingh@Gurnoors-MacBook-Pro.local>
2022-02-09 10:15:38 -0500

    changed background color of loading to white

--- src/ui/screens/loading/loading.styles.js
+++ src/ui/screens/loading/loading.styles.js
@@ -4,7 +4,7 @@ export default StyleSheet.create({
     container: {
       height: '100%',
       width: '100%',
-      backgroundColor: 'rgb(220,20,60)',
+      backgroundColor: 'white',
       alignItems: 'center',
       justifyContent: 'center',
     },
