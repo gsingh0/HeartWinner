836cc2ac39fd0b169d9f2be6caa0ab3226ef0fb1
Gurnoor Singh <gurnoorsingh@Gurnoors-MacBook-Pro.local>
2022-02-09 01:27:21 -0500

    changed from bottom nav to top nav

--- src/ui/components/navigators/TabNavigators.js
+++ src/ui/components/navigators/TabNavigators.js
@@ -1,13 +1,20 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
 import Home from '../../screens/home/Home';
 import Logout from '../../screens/settings/Logout';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 export default function TabNavigators() {
     return (
         <Tab.Navigator
+            shifting={true}
+            labeled={true}
+            // activeColor="rgb(220,20,60)"
+            style={{}}
+            // inactiveColor="black"
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
                 let iconName;
@@ -16,18 +23,19 @@ export default function TabNavigators() {
                     iconName = focused
                     ? 'home-heart'
                     : 'home-heart';
-                    return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+                    return <MaterialCommunityIcons style={{marginTop: 5}} name={iconName} size={20} color={color} />;
                 } else if (route.name === 'Logout') {
                     iconName = focused ? 'logout' : 'logout';
-                    return <MaterialIcons name={iconName} size={size} color={color} />;
+                    return <MaterialIcons style={{marginTop: 5}} name={iconName} size={20} color={color} />;
                 }
     
                 // You can return any component that you like here!
                 },
-                tabBarActiveBackgroundColor: 'rgb(220,20,60)',
-                tabBarInactiveBackgroundColor: 'rgb(220,20,60)',
-                tabBarActiveTintColor: 'white',
-                tabBarInactiveTintColor: 'black',
+                tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: "white",
+                swipeEnabled: true,
+                tabBarIndicatorStyle: {backgroundColor: "black",},
+                tabBarStyle: {backgroundColor: "rgb(220,20,60)"},
             })}>
             <Tab.Screen name='Home' component={Home} options={{ headerShown: false }}/>
             <Tab.Screen name='Logout' component={Logout} options={{ headerShown: false }}/>
